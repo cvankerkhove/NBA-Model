ad5c7e66c904a40fb5ee69428d9d4521032fb942
Chris VanKerkhove <cjv47@cornell.edu>
2021-03-16 22:37:01 -0400

    Adding Webscrapping Files
    Files contain webscraping functionality for getting stats from basketball-reference-.com

---
+++ NBA_Games_Scrape.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from game_log_scrape import team_log
+
+class Game_Info:
+    #class variables:
+    #date: string of the date of game
+    #team: 3 letter string of team
+    #loc: location of the game for team (home or away)
+    #team_score: score of the given team
+    #o_score: score of opponent
+    #team_basic: basic stats total for team
+    #team_advanced: advanced stats total for team
+
+    #if player_data() is called
+    #basic_player_data: df of basic stats for all player
+    #advanced_player_data: df of advanced stats for all players
+    def __init__(self, game_log, date):
+        """
+        Arg(s):
+            game_log: game log from a single game for a team
+            date: string of date of the game
+        """
+        self.date = date
+        self.team = game_log[7]
+        self.opponent = game_log[8]
+        if game_log[0]:
+            self.loc = 'Away'
+        else:
+            self.loc = 'Home'
+        #score of each team
+        #team score
+        self.team_score = int(game_log[2]['PTS'])
+        #opponent score
+        self.o_score = int(game_log[5]['PTS'])
+        #team stats
+        self.team_basic = game_log[2]
+        self.team_advanced = game_log[4]
+        #opponent stats
+        self.o_basic = game_log[5]
+        self.o_advanced = game_log[6]
+
+    def player_data(self, game_log):
+        """
+        Populates class variables with pandas
+        dataframe objects of the stats
+        """
+        #basic player data
+        b_data = game_log[1]
+        b_data['Players'] = b_data['Players'][:len(b_data['MP'])]
+        b_data.pop('Team', None)
+        df = pd.DataFrame(b_data)
+        self.basic_player_data = df
+
+        #advanced player data
+        a_data = game_log[3]
+        a_data['Players'] = a_data['Players'][:len(a_data['MP'])]
+        a_data.pop('Team', None)
+        df = pd.DataFrame(a_data)
+        self.advanced_player_data = df
+
+
+
+
+#Script for getting games
+def func(team, date, year):
+    # script to create list of game_info objects for a single team
+    #log = team_log(team, date, date)
+    log = team_log(team, date, '03-02-2021', year)
+    games = {}
+
+    for key, val in log.items():
+        date = key[11:19]
+        date = date[4:6] + '-' + date[6:] + '-' + date[:4]
+        game = Game_Info(val, date)
+        game.player_data(val)
+        games[date] = game
+        print(game.team, game.team_score)
+        print(game.opponent, game.o_score)
+        print(game.basic_player_data)
+        #print(game.team_basic)
+        #print(game.advanced_player_data)
+        #print(game.o_basic)
+        #print(game.o_advanced)func('LAL', '01-06-2006', '2006')
+
+
+func('LAL', '01-06-2006', '2006')
---
+++ game_log_scrape.py
@@ -0,0 +1,164 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_box_score(team, opponent, game, box_type):
+    #TODO: Return who is inactive
+    #refs?
+    """
+    Helper function that retrieves basic or advanced
+    Box score stats for specificed team from boxscore
+    page on basketball-reference.com
+
+    Arg(s):
+        team: string representation of team
+        game: string of the game
+        box_type: string that tells if its basic or advanced box score
+    """
+    URL = 'https://www.basketball-reference.com'
+    URL = URL + game
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ###PROCESS FOR RETRIEVING BOX SCORE
+    #home box score basic content
+    team_id = 'box-' + team + '-game-' + box_type
+    results = soup.find(id = team_id)
+
+    #Process for creating empty data structure
+    #table header
+    header = results.find('thead')
+    key_dict ={}
+    #home team data
+    data = {'Team': team, 'Players': []}
+    #columns in the header
+    cols = header.find_all('th')
+    for col in cols:
+        key_dict[col.get('data-stat')] = col.text
+        data[col.text] = []
+    del data['']
+    if box_type == 'basic':
+        del data['Basic Box Score Stats']
+    else:
+        del data['Advanced Box Score Stats']
+    del data['Starters']
+
+    #Process for populating data tables
+    #table with information
+    body = results.find('tbody')
+
+    t_data = body.find_all('tr')
+    for row in t_data:
+        if row.get('class') != ['thead']:
+            player = row.find('th')
+            data['Players'].append(player.text)
+            col = row.find_all('td')
+            for col in col:
+                if col.get('data-stat') in key_dict.keys():
+                    data[key_dict[col.get('data-stat')]].append(col.text)
+    #getting stat team totals
+    team_tot = {}
+    footer = results.find('tfoot')
+    row = footer.find_all('td')
+    for x in row:
+        team_tot[key_dict[x.get('data-stat')]] = x.text
+
+    #TODO: getting team inactives
+    x = soup.find('span')
+    #TODO: getting opponent inactives
+
+    # getting opponent totals
+    o_id = 'box-' + opponent + '-game-' + box_type
+    results = soup.find(id = o_id)
+    opponent_tot = {}
+    footer = results.find('tfoot')
+    row = footer.find_all('td')
+    for x in row:
+        opponent_tot[key_dict[x.get('data-stat')]] = x.text
+
+    return data, team_tot, opponent_tot
+
+
+def team_log(team, start = '12-20-2020', end = '03-20-2021', year = '2021'):
+    """
+    Takes an input of a string representation of a
+    team and returns
+    Arg(s):
+        team: 3 letter string representation of Team
+        start: start date to begin collecting games
+            in the form of 'MM-DD-YYYY'
+        end date: last game date to collect games
+            in the form of 'MM-DD-YYYY'
+        year: string of year of season YYYY
+    """
+    #helpful dictionaries
+    num_to_str = {12: 'december', 1: 'january', 2: 'february', 3: 'march', 4: 'april', 5: 'may',
+                     6: 'june', 7: 'july', 8: 'august', 9: 'september', 10: 'october', 11: 'november'}
+    next_ = {'december': 'january', 'january': 'february', 'february': 'march', 'march': 'april',
+           'april': 'may', 'may': 'june', 'june': 'july', 'july': 'august', 'august': 'september',
+           'september' : 'october', 'october': 'november', 'november': 'december'}
+    month_days = {'december': 31, 'january': 31, 'february': 28, 'march': 31}
+    months = []
+    game_logs = {}
+    #finding the months over which to grab data
+    start_month = num_to_str[int(start[:2])]
+    end_month = num_to_str[int(end[:2])]
+    current = start_month
+    months.append(current)
+    while current != end_month:
+        current = next_[current]
+        months.append(current)
+    #starting dat (first month)
+    start_day = int(start[3:5])
+    #ending day (first month)
+    if len(months) == 1:
+        end_day = int(end[3:5])
+    else:
+        end_day = 31
+
+    #iterating through different months
+    stop = False
+    while not stop:
+        month = months[0]
+        URL = 'https://www.basketball-reference.com/leagues/NBA_' + year + '_games-' + month + '.html'
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        #content
+        results = soup.find(id = 'all_schedule')
+        body = results.find('tbody')
+        t_data = body.find_all('tr')
+
+        #Grabbing all games for given month over specified days
+        for row in t_data:
+            #ensure row has data
+            if row.get('class') != 'thead':
+                teams = row.find_all('a')
+                #ensure game has been played
+                if len(teams) < 4:
+                    break
+                game = teams[3].get('href')
+                #only games after specificed start date
+                if start_day <= int(game[17:19]) <= end_day:
+                    away = teams[1].get('href')[7:10]
+                    home = teams[2].get('href')[7:10]
+                    if team == home or team == away:
+                        if team == home:
+                            opponent = away
+                        else:
+                            opponent = home
+                        visitor = (team == away)
+                        print(game)
+                        #retrieving basic data from game
+                        b_data, b_tot, b_o_tot = get_box_score(team, opponent, game, 'basic')
+                        #retrieving advanced data from game
+                        a_data, a_tot, a_o_tot = get_box_score(team, opponent, game, 'advanced')
+                        game_logs[game] = [visitor, b_data, b_tot, a_data, a_tot, b_o_tot, a_o_tot, team, opponent]
+
+        #starting new month
+        start_day = 1
+        months.pop(0)
+        if len(months) == 1:
+            end_day = int(end[3:5])
+        elif len(months) == 0:
+            stop = True
+
+    return game_logs
